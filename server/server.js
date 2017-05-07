34d4e86247e28eecf6435f16b09d5b2b5d147489
Stephane Moreau <st.moreau@outlook.com.gr>
2017-05-07 16:23:55 +0100

    create models and different db urls depending on NODE_ENV

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var app = express();
 var api = require('./api/api');
+var config = require('./config/config');
 
+// db.url is different depending on NODE_ENV
+require('mongoose').connect(config.db.url);
+
+// setup the app middlware
 require('./middleware/appMiddlware')(app);
 
-app.use('/api', api);
+// setup the api
+app.use('/api/', api);
 
+// export the app for testing
 module.exports = app;
